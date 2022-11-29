ea7dabbd852c7d8221453d7ced3192869cfe1548
TUANSPC\Dell Vostro 14 <19110494@student.hcmute.edu.vn>
2022-11-29 13:55:13 +0700

    up

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','..', 'public'));
+    res.sendFile(path.join(__dirname, '..','..', 'public', 'index.html'));
 })
 
 route(app);
