86ae72855e1b34e4c65db111be73c8856f2f9a41
lancer-to-learn <docaominhchik17.125@gmail.com>
2022-11-30 00:59:32 +0700

    change

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,9 @@
 const userRoute = require('./user.route');
-const { checkLoggedIn } = require('../config/security/login');
+//const { checkLoggedIn } = require('../config/security/login');
 
 
 function route(app) {
-    app.use('/',checkLoggedIn, userRoute);
+    app.use('/', userRoute);
 };
 
 module.exports = route;
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -3,23 +3,20 @@ const { test } = require('../app/constrollers.js/user');
 
 const passport = require('passport')
 const router = express.Router()
-
-
-
 router.get('/test', test);
-router.get('/auth/google', passport.authenticate('google', {
-    scope: ['email'],
-}))
-router.get('/auth/google/callback', passport.authenticate('google', {
-    failureRedirect: '/failure',
-    successRedirect: '/',
-    session: true,
-}), (req, res) => {
-    console.log('Google is called us back!')
-})
-router.get('/auth/logout', (req, res) => {
-    req.logOut()
-    return res.redirect('/')
-})
+// router.get('/auth/google', passport.authenticate('google', {
+//     scope: ['email'],
+// }))
+// router.get('/auth/google/callback', passport.authenticate('google', {
+//     failureRedirect: '/failure',
+//     successRedirect: '/',
+//     session: true,
+// }), (req, res) => {
+//     console.log('Google is called us back!')
+// })
+// router.get('/auth/logout', (req, res) => {
+//     req.logOut()
+//     return res.redirect('/')
+// })
 
 module.exports = router;
