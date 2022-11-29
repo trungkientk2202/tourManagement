0a1d4db97a35897f56b3383dbbf557653362777b
TUANSPC\Dell Vostro 14 <19110494@student.hcmute.edu.vn>
2022-11-29 11:40:56 +0700

    config cont

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public'));
+    res.sendFile(path.join(__dirname, '..','..', 'public'));
 })
 
 route(app);
