4256586959cba065671e780a16bc72e01b7f1512
Levantuan0512 <tuan05122001@gmail.com>
2022-12-19 22:19:09 +0700

    tour/admin - 20221219

--- server/src/routes/image.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { uploadImage, uploadImageHandler, getImage } = require('../app/constrollers/image');
-const { checkLoggedIn } = require('../config/security/auth');
-const router = express.Router();
-
-router.post('/upload',checkLoggedIn, uploadImage.single('file'), uploadImageHandler);
-router.get('/:id',checkLoggedIn, getImage);
-
-module.exports = router;
